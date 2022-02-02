df1f626de1f9039b61c2ce1b0636eb424f6e44ff
adrianlm2 <adrianl0@chicagobooth.edu>
2022-02-02 02:30:23 +0000

    addition

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -12,4 +12,20 @@ class MathController < ApplicationController
     @result = @first - @second
     render({ :template => "math_templates/subtraction_results.html.erb"})
   end
+
+  def addition_form
+
+    render({ :template => "math_templates/addition_form.html.erb" })
+end
+
+def addition_results
+
+  @add1 = params.fetch("add1").to_f
+  @add2 = params.fetch("add2").to_f
+  @result_add = @add1 + @add2
+  render({ :template => "math_templates/addition_results.html.erb"})
+end
+
+
+
 end
